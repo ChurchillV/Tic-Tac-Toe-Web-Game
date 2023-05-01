a09539b97bd8b2edb0df2cbfce265241d099ad93
ChurchillV <vinchurch70@gmail.com>
2023-05-01 00:28:21 +0000

    Commenting Js code

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ let alert = document.querySelector('#player-turn');
 let currentPlayer = 'X';
 let free_spaces = 9;
 
+//This is executed every time a box is clicked
 boxes.forEach(function(box){
     box.addEventListener('click',function(){
-        if (box.textContent !== ""){
+        if (box.textContent !== ""){  // If the box already has a value, nothing is done
             return;
         }
-        box.textContent = currentPlayer;
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        box.textContent = currentPlayer;    //Otherwise a value is assigned to it
+        currentPlayer = currentPlayer === "X" ? "O" : "X";      //Tenary operator to change the current player (If X, change to O, else make it X)
         alert.textContent = "Player " + currentPlayer + "'s turn";
         free_spaces -= 1;
 
         if (free_spaces <= 6) {
             const winning_combinations = [
-                [0,1,2], [3,4,5], [6,7,8],
-                [0,3,6], [1,4,7], [2,5,8],
-                [0,4,8], [2,4,6] ];
+                [0,1,2], [3,4,5], [6,7,8],  //rows
+                [0,3,6], [1,4,7], [2,5,8],  //columns
+                [0,4,8], [2,4,6] ];         //diagonals
             
             for (let i = 0; i < winning_combinations.length; i++) {
                 const [a, b, c] = winning_combinations[i];
