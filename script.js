7c5acbea87b89561b2e96f8e9246e40aecf051c6
ChurchillV <vinchurch70@gmail.com>
2023-05-01 00:21:53 +0000

    Perfected winner checking algorithm

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const boxes = document.querySelectorAll('.box');
 const x_o = document.querySelectorAll('.char');
 let alert = document.querySelector('#player-turn');
 let currentPlayer = 'X';
+let free_spaces = 9;
 
 boxes.forEach(function(box){
     box.addEventListener('click',function(){
@@ -11,23 +12,20 @@ boxes.forEach(function(box){
         box.textContent = currentPlayer;
         currentPlayer = currentPlayer === "X" ? "O" : "X";
         alert.textContent = "Player " + currentPlayer + "'s turn";
+        free_spaces -= 1;
+
+        if (free_spaces <= 6) {
+            const winning_combinations = [
+                [0,1,2], [3,4,5], [6,7,8],
+                [0,3,6], [1,4,7], [2,5,8],
+                [0,4,8], [2,4,6] ];
+            
+            for (let i = 0; i < winning_combinations.length; i++) {
+                const [a, b, c] = winning_combinations[i];
+                if(boxes[a].textContent && boxes[a].textContent === boxes[b].textContent && boxes[b].textContent === boxes[c].textContent) {
+                    alert.textContent = "Player " + boxes[a].textContent + " wins!!";
+                }
+            }
+        }
     });
 });
-
-function checkWinner() {
-    const winning_combinations = [
-        [0,1,2], [3,4,5], [6,7,8],
-        [0,3,6], [1,4,7], [2,5,8],
-        [0,4,8], [2,4,6] ];
-    
-    for (let i = 0; i < winning_combinations.length; i++) {
-        const [a, b, c] = winning_combinations[i];
-        if(boxes[a].textContent && boxes[a].textContent === boxes[b].textContent && boxes[b].textContent === boxes[c].textContent) {
-            alert.textContent = "Player " + boxes[a].textContent + " wins!!";
-            boxes.forEach(function(box){
-                box.textContent = "";
-            });
-        }
-        return null;
-    }
-}
