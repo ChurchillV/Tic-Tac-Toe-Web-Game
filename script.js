4b9c0e604bedf517970e137b4f2c2e95f87dd42b
ChurchillV <vinchurch70@gmail.com>
2023-05-03 21:24:16 +0000

    Added the draw functionality

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const boxes = document.querySelectorAll('.box');
 let info_box = document.getElementById('player-turn');
 let currentPlayer = 'X';
 let free_spaces = 9;
+let taps = 0;
 let pop_up = document.getElementById('popup');
 let winner_msg = document.getElementById('winnermessage');
 let container = document.getElementById('container-fluid');
 let scoreX = document.getElementById('x-score');
 let scoreO = document.getElementById('o-score');
-const img = document.getElementById('winners-trophy');
-var x_score = 0;
-var o_score = 0;
+let x_score = 0;
+let o_score = 0;
 scoreX.textContent = x_score;
 scoreO.textContent = o_score;
 
@@ -29,10 +29,11 @@ boxes.forEach(function(box){
             [0,4,8], [2,4,6] ];         //diagonals
 
         if (free_spaces <= 6) {
-            
+            let isDraw = true;
             for (let i = 0; i < winning_combinations.length; i++) {
                 const [a, b, c] = winning_combinations[i];
                 if(boxes[a].textContent && boxes[a].textContent === boxes[b].textContent && boxes[b].textContent === boxes[c].textContent) {
+                    isDraw = false;
                     winner_msg.textContent = "Player " + boxes[a].textContent + " wins!!";
                     info_box.textContent = winner_msg.textContent;
                     
@@ -44,24 +45,24 @@ boxes.forEach(function(box){
 
                     //Update score
                     if(boxes[a].textContent == 'X'){
-                        scoreX.textContent = x_score + 1;
+                        x_score += 1;
+                        scoreX.textContent = x_score;
                     }
                     else {
-                        scoreO.textContent = o_score + 1;
+                        o_score += 1;
+                        scoreO.textContent = o_score;
                     }
                 }
-                else if (free_spaces == 0) {
-                    winner_msg.textContent = "Draw!!";
-                    img.src = "./images/handshake.png";
-                    info_box.textContent = winner_msg.textContent;
-
-                    //Make the pop-up appear
-                    pop_up.style.visibility = "visible";
-                    pop_up.style.top = "50%";
-                    pop_up.style.transform = "translate(-50%, -50%) scale(1)";
-                    container.style.filter = "blur(5px)";
-                }
-                
+            }
+            if (isDraw && free_spaces === 0) {
+                //document.getElementById('winners-trophy').src = "./handshake.png"
+                winner_msg.textContent = "Draw!!";
+                info_box.textContent = winner_msg.textContent;
+                pop_up.style.visibility = "visible";
+                pop_up.style.top = "50%";
+                pop_up.style.transform = "translate(-50%, -50%) scale(1)";
+                container.style.filter = "blur(5px)";
+                free_spaces = 9;
             }
         }
     });
